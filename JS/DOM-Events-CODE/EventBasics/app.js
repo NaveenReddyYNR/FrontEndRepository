ba6bccb613449df83f298c442a647bb0f6ad1c88
LAPTOP-4MNKJF99\aramc <aramcharan.d@gmail.com>
2022-04-27 10:54:08 +0530

    add

---
+++ JS/DOM-Events-CODE/EventBasics/app.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector("#v2");
+
+btn.onclick = function () {
+  console.log("YOU CLICKED ME!");
+  console.log("I HOPE IT WORKED!!");
+};
+
+function scream() {
+  console.log("AAAAAHHHHH");
+  console.log("STOP TOUCHING ME!");
+}
+
+btn.onmouseenter = scream;
+
+document.querySelector("h1").onclick = () => {
+  alert("you clicked the h1!");
+};
+
+const btn3 = document.querySelector("#v3");
+btn3.addEventListener("click", function () {
+  alert("CLICKED!");
+});
+
+function twist() {
+  console.log("TWIST!");
+}
+function shout() {
+  console.log("SHOUT!");
+}
+
+const tasButton = document.querySelector("#tas");
+
+// tasButton.onclick = twist; // Only shout fn is applied.
+// tasButton.onclick = shout;
+
+tasButton.addEventListener("click", twist); // with this both fn's will execute.
+tasButton.addEventListener("click", shout);
+//tasButton.addEventListener("click", shout, { once: true }); // shout fn will execute only once.
--- JS/DOM-Intro-CODE/Selecting/app.js
+++ JS/DOM-Intro-CODE/Selecting/app.js
@@ -24,3 +24,40 @@ banner.style.width = "500px";
 const ad = document.querySelector("img:nth-of-type(3)");
 document.querySelector('a[title="Java"');
 ad.style.height = "10px";
+
+// properties and methods
+
+const h1 = document.querySelector("h1");
+const h12 = document.querySelector("h1:nth-of-type(2)");
+const h13 = document.querySelector("h1:nth-of-type(3)");
+
+h1.getAttribute("class");
+// h1.setAttribute('class','class1');
+// h1.setAttribute('class','class2');
+
+h1.classList.add("class1"); // we have add, remove, contains, toggle
+h1.classList.add("class2"); // we have add, remove, contains, toggle(class to toggle{on\off})
+h12.classList.add("class1", "class2");
+h13.classList.add("class1", "class2");
+
+const h3 = document.createElement("h3");
+h3.innerText = "I am H3";
+document.body.appendChild(h3); // appent element at last child inside body.
+h3.append(" appendText");
+h3.prepend("prependText ");
+
+//h1.appendChild(h3); // to add element inside targeted element. <h1> <h3>appendChild</h3> </h1>
+
+const t1 = document.createElement("p");
+t1.innerText = "I am p1";
+
+//h12.insertAdjacentElement("beforebegin", t1);
+h12.insertAdjacentElement("beforeend", t1); // child of h12
+//h12.insertAdjacentElement("afterbegin", t1);
+// h12.insertAdjacentElement("afterend", t1);
+
+//h12.after(t1);
+// h12.before(t1);
+
+h12.removeChild(t1);
+h12.remove();
