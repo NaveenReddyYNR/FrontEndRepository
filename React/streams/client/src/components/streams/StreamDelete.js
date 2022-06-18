c0de14e2cbe599cd33ef2c28f71da66503ff582e
LAPTOP-4MNKJF99\aramc <aramcharan.d@gmail.com>
2022-06-18 21:50:01 +0530

    final

--- React/streams/client/src/components/streams/StreamDelete.js
+++ React/streams/client/src/components/streams/StreamDelete.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import Modal from "../Modal";
-import history from "../../history";
-import { fetchStream, deleteStream } from "../../actions";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import Modal from '../Modal';
+import history from '../../history';
+import { fetchStream, deleteStream } from '../../actions';
 
 class StreamDelete extends React.Component {
   componentDidMount() {
@@ -12,6 +12,7 @@ class StreamDelete extends React.Component {
 
   renderActions() {
     const { id } = this.props.match.params;
+
     return (
       <React.Fragment>
         <button
@@ -20,38 +21,40 @@ class StreamDelete extends React.Component {
         >
           Delete
         </button>
-        <Link to="/" className="ui primary button">
+        <Link to="/" className="ui button">
           Cancel
         </Link>
       </React.Fragment>
     );
   }
+
   renderContent() {
     if (!this.props.stream) {
-      return "Are you sure you want to delete this stream?";
+      return 'Are you sure you want to delete this stream?';
     }
-    return (
-      <p>
-        Are you sure you want to delete this stream with title:{" "}
-        <b>{this.props.stream.title}</b>
-      </p>
-    );
+
+    return `Are you sure you want to delete the stream with title: ${
+      this.props.stream.title
+    }`;
   }
+
   render() {
     return (
       <Modal
         title="Delete Stream"
         content={this.renderContent()}
         actions={this.renderActions()}
-        onDismiss={() => history.push("/")}
+        onDismiss={() => history.push('/')}
       />
     );
   }
 }
+
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream, deleteStream })(
-  StreamDelete
-);
+export default connect(
+  mapStateToProps,
+  { fetchStream, deleteStream }
+)(StreamDelete);
--- React/streams/client/src/components/streams/StreamList.js
+++ React/streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchStreams } from "../../actions";
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -27,13 +27,15 @@ class StreamList extends React.Component {
   }
 
   renderList() {
-    return this.props.streams.map((stream) => {
+    return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
@@ -44,7 +46,7 @@ class StreamList extends React.Component {
   renderCreate() {
     if (this.props.isSignedIn) {
       return (
-        <div style={{ textAlign: "right" }}>
+        <div style={{ textAlign: 'right' }}>
           <Link to="/streams/new" className="ui button primary">
             Create Stream
           </Link>
@@ -64,12 +66,15 @@ class StreamList extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     streams: Object.values(state.streams),
     currentUserId: state.auth.userId,
-    isSignedIn: state.auth.isSignedIn,
+    isSignedIn: state.auth.isSignedIn
   };
 };
 
-export default connect(mapStateToProps, { fetchStreams })(StreamList);
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);
